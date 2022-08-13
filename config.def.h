4131f0129ab4ab6ba9269980a351279d04bbd609
Miguel Hernandez <hernandezmiw@gmail.com>
2022-08-12 20:19:10 -0500

    Fix identation

--- config.def.h
+++ config.def.h
@@ -26,8 +26,8 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	/* class       instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",      NULL,       NULL,       0,            1,           -1 },
 	{ "Chromium",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
