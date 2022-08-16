edb64bc7cb4232cd62f61b1b991b04d40b3fcf1e
Miguel Hernandez <hernandezmiw@gmail.com>
2022-08-15 20:45:19 -0500

    Modify rules

--- config.def.h
+++ config.def.h
@@ -33,8 +33,7 @@ static const Rule rules[] = {
 	 */
 	/* class       instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",      NULL,       NULL,       0,            1,           -1 },
-	{ "KeePassXC", NULL,       NULL,       0,            1,           -1 },
-	{ "Chromium",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "KeePassXC", NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
